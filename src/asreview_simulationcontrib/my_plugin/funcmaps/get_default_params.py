8e87decaa1ac9505a940c9e1ba20de16ef58d64d
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-12-12 11:51:01 +0100

    simplified the directory structure

--- src/asreview_simulationcontrib/my_plugin/funcmaps/get_default_params.py
+++
@@ -1,7 +0,0 @@
-from asreview_simulationcontrib.my_plugin.lib.ofn.ofn_custom_params import get_ofn_custom_params
-
-
-funcmap_get_default_params = {
-    "ofn-custom": get_ofn_custom_params,
-    "bal-other": get_ofn_custom_params,
-}
