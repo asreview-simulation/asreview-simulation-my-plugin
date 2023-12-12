09650fd753f9aa2d82e99449d36826935ed6ab9d
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-12-12 12:48:49 +0100

    illustration ofn now using the expected interface

--- src/asreview_simulationcontrib/my_plugin/ofn_custom_fun.py
+++ src/asreview_simulationcontrib/my_plugin/ofn_custom_fun.py
@@ -1,2 +1,8 @@
-def ofn_plugin_fun():
-    return -999
+import math
+
+
+def ofn_custom_fun(_project_path, *, param_1, param_2):
+    # (not a real objective function, just for illustration)
+    if param_2 is True:
+        return param_1 ** 2
+    return math.sqrt(param_1)
